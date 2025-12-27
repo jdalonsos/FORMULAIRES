6aa19cb7a7923f6947f68a8820b8278ff99eeb28
jdalonsos@unal.edu.co <jdalonsos@unal.edu.co>
2025-12-27 11:47:28 +0100

    Ajoute from_analyze to main

--- project_form_auto/app/main.py
+++ project_form_auto/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from app.routers.form_analyzer import router as form_analyze_router
 from app.routers.form_detect import router as form_detect_router
 from app.routers.health import router as health_router
 
@@ -7,3 +7,4 @@ app = FastAPI(title="Web Form Detector", version="0.1.0")
 
 app.include_router(health_router)
 app.include_router(form_detect_router)
+app.include_router(form_analyze_router)
