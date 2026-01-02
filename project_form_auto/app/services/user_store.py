c0b2793805b0c4860e8f982d6fd3e00ec045f25b
Amelchrb <cherbiamel@gmail.com>
2026-01-02 18:50:57 +0100

    FIX : convention python ajout de l'underscore

--- project_form_auto/app/services/user_store.py
+++ project_form_auto/app/services/user_store.py
@@ -4,38 +4,38 @@ from app.models.schemas import UserData, UserPatchRequest
 
 # Les données utilisateur sont stockées en mémoire pour simplifier l’architecture
 
-USER: Optional[UserData] = None
+_USER: Optional[UserData] = None
 
 
 def get_user() -> Optional[UserData]:
-    return USER
+    return _USER
 
 
 def create_user(user_data: UserData) -> UserData:
-    global USER
-    if USER is not None:
+    global _USER
+    if _USER is not None:
         raise ValueError("Un utilisateur existe déjà.")
-    USER = user_data
-    return USER
+    _USER = user_data
+    return _USER
 
 
 # Logique put : je renvoie l'utilisateur un utilisateur complet. 
 def replace_user(user_data: UserData) -> UserData:
-    global USER
-    if USER is None:
+    global _USER
+    if _USER is None:
         raise ValueError("Aucun utilisateur n'existe pour le remplacer.")
-    USER = user_data
-    return USER
+    _USER = user_data
+    return _USER
 
 
 def patch_user(partial: UserPatchRequest) -> UserData:
-    global USER
-    if USER is None:
+    global _USER
+    if _USER is None:
         raise ValueError("Aucun utilisateur n'existe pour le modifier.")
 
-    current = USER.model_dump()  # Pour récupérer un dicctionnaire python.
+    current = _USER.model_dump()  # Pour récupérer un dicctionnaire python.
     updates = partial.model_dump(exclude_unset=True)
 
     current.update(updates)
-    USER = UserData(**current)
-    return USER
+    _USER = UserData(**current)
+    return _USER
